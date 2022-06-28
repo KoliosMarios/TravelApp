3e525e7f781f9981f770a1db1912f12b66496d64
KoliosMarios <koliosmarios@gmail.com>
2022-06-28 10:44:25 +0300

    render info to page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import "./App.css";
+import { useState } from "react";
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const [sites, setSites] = useState([]);
+
+  const getSites = async (str) => {
+    const colRef = collection(db, str);
+    const data = await getDocs(colRef);
+    setSites(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
 
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <button
+        onClick={() => {
+          getSites("athens");
+        }}
+      >
+        Get Sites
+      </button>
+      {sites.map((site) => {
+        return (
+          <div key={site.id}>
+            <h1>Name: {site.name}</h1>
+            <p>Info: {site.info}</p>
+            <img src={site.img} alt="img" />
+          </div>
+        );
+      })}
     </div>
   );
 }
