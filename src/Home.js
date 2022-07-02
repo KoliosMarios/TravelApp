601535ce9d3d7d46e8d0eca6e748b0ce8b1454bc
KoliosMarios <koliosmarios@gmail.com>
2022-07-02 18:42:58 +0300

    add welcome page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import "./styles/Home.css";
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home_container">
+      <div className="home">
+        <h1 className="home_title">Welcome!</h1>
+        <h3 className="home_subtitle">
+          Get the most famous sights of some of the most touring cities of
+          Europe.
+        </h3>
+        <p className="home_para">
+          Just select the city you want to visit and let our list drive you
+          through the most iconic attractions in that destination
+        </p>
+      </div>
     </div>
   );
 }
