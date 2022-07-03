f66ebc4a7e4968a0f0d98a31c3f5c4d4002af4a4
KoliosMarios <koliosmarios@gmail.com>
2022-07-03 12:34:15 +0300

    add about page

--- src/About.js
+++ src/About.js
@@ -1,7 +1,17 @@
+import "./styles/Home.css";
+
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="home_container">
+      <div className="home">
+        <h1 className="home_title">Some information about our team</h1>
+        <p className="about_para">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+          cum, recusandae officia eveniet provident voluptas expedita reiciendis
+          pariatur aperiam animi voluptatibus, nostrum sapiente culpa atque
+          doloribus debitis neque hic magni.
+        </p>
+      </div>
     </div>
   );
 }
